0bf47881eec9de4258792389564d7489f79ee1e8
Emiko Esther S <graceupongraceemi@gmail.com>
2023-08-08 00:03:34 -0700

    Tab function updated in Tab.jsx of components

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useSnapshot } from 'valtio';
 
-const Tab = () => {
-  return <div>Tab</div>;
+import state from '../store';
+
+const Tab = ({ tab, isFilerTab, isActiveTab, handleClick }) => {
+  const snap = useSnapshot(state);
+
+  return (
+    <div
+      key={tab.name}
+      className={`tab-btn ${
+        isFilerTab ? 'rounded-full glassmorhism' : 'rounded-4'
+      }`}
+    ></div>
+  );
 };
 
 export default Tab;
