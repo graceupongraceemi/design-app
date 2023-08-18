feb8cfb5205a8add91fdc908cb2e963a819cf404
Emiko Esther S <graceupongraceemi@gmail.com>
2023-08-18 12:32:32 -0700

    server side dependnecies installed

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -39,11 +39,28 @@ const Customizer = () => {
       case 'filepicker':
         return <FilePicker file={file} setFile={setFile} readFile={readFile} />;
       case 'aipicker':
-        return <AIPicker />;
+        return <AIPicker 
+          prompt={prompt}
+          setPrompt={setPrompt}
+          generatingImg={generatingImg}
+          handleSubmit={handleSubmit}
+        />;
       default:
         return null;
     }
   };
+
+  const handleSubmit = async (type) => {
+    if(!prompt) return alert('Please enter a prompt')
+
+    try {
+      // call our backend to generate an ai image!
+    } catch (error) {
+      alert(error)
+    } finally
+      setGeneratingImg(false)
+      setActiveEditorTab('')
+  }
 
   const handleDecals = (type, result) => {
     const decalType = DecalTypes[type];
